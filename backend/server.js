6073e5132f52e6264fd9082a42efc49e80d62add
RohitVerma2003 <rvermajio78300@gmail.com>
2025-04-10 19:20:57 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import { app, server } from './Socket/socket.js';
+import path from 'path';
 
 const PORT = 5000;
 app.use(express.json());
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname , "/frontend/dist")));
+app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"));
+})
 
 server.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`);
